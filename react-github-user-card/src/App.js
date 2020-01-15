276b9c4055000a2f50aa7ede87125f41b9c1ed9b
Hiter Harris <hiterharris@comcast.net>
2020-01-14 19:18:13 -0500

    removed unused code

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import FollowerList from './components/FollowerList';
 import Profile from './components/Profile';
-import { Route, Link } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import './App.css';
 
 export default class App extends React.Component {
--- react-github-user-card/src/components/Profile.js
+++ react-github-user-card/src/components/Profile.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import FollowerList from './FollowerList';
 import axios from 'axios';
-import { Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import '../App.css';
 
 export default class App extends React.Component {
